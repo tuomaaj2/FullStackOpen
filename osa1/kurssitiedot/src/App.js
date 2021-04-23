5a49f97df4f0d7624f2722a1db1d5072ed01b9f8
Juho-Topias Tuomaala <juho-topias@hotmail.com>
2021-04-23 16:58:19 +0300

    step3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,23 +2,30 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
   <>
     <h1>{props.course}</h1>
@@ -27,16 +34,18 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
   <>
-    <Part part={props.part1} exercises={props.exercises1}/>
-    <Part part={props.part2} exercises={props.exercises2}/>
-    <Part part={props.part3} exercises={props.exercises3}/>
+    <Part part={props.part1.name} exercises={props.part1.exercises}/>
+    <Part part={props.part2.name} exercises={props.part2.exercises}/>
+    <Part part={props.part3.name} exercises={props.part3.exercises}/>
   </>
   )
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <>
       <p>
@@ -47,6 +56,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   const total = props.exercises1 + props.exercises2 + props.exercises3
   return (
     <>
